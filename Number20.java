4efe9f8abcfa8f47934e9121c58e277ee05ff89e
Glydel Mary Rose Visto <glydelmaryrose@gmail.com>
2025-12-05 15:16:01 +0800

    Add files via upload

---
+++ Number20.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Number20 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Number20
+{
+    public static void main(String[] args) {
+        int[] array = {10, 22, 15, 24, 30};
+        for (int num : array) {
+            if (num % 2 == 0 && num > 20) {
+                System.out.println(num);
+            }
+        }
+    }
+}
