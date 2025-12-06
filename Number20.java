6f323075269b9d1b8e319750642cb72e56492206
Glydel Mary Rose Visto <glydelmaryrose@gmail.com>
2025-12-07 04:02:31 +0800

    Update Number20.java

--- Number20.java
+++ Number20.java
@@ -9,6 +9,7 @@ public class Number20
 {
     public static void main(String[] args) {
         int[] array = {10, 22, 15, 24, 30};
+        
         for (int num : array) {
             if (num % 2 == 0 && num > 20) {
                 System.out.println(num);
